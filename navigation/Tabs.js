8c162f4d78ea9b296b51323ec929ad83b8ba96b2
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-06-30 22:46:54 +0900

    get title from Tab

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
@@ -7,11 +8,26 @@ import Favs from "../screens/Favs";
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-  <Tabs.Navigator>
-    <Tabs.Screen name="Movies" component={Movies} />
-    <Tabs.Screen name="Tv" component={Tv} />
-    <Tabs.Screen name="Search" component={Search} />
-    <Tabs.Screen name="Favs" component={Favs} />
-  </Tabs.Navigator>
-);
+export default ({ navigation, route }) => {
+  //Stack nav가 Tab nav에게 전달해주는 prop
+  //이 prop을 통해 부모와 소통할 수 있음
+  //   useEffect(() => {
+  //     navigation.setOptions({ title: "Hello from the Tabs" });
+  //   }, []);
+
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Movies";
+  //route prop 우리가 어디있는지 알려줌
+  //layoutEffect은 useEffect와 비슷한데 레이아웃 변경이 다 끝난 후에 작동한다는 차이점
+  useEffect(() => {
+    navigation.setOptions({ title: routeName });
+  }, [route]);
+
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Movies" component={Movies} />
+      <Tabs.Screen name="TV" component={Tv} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Favourites" component={Favs} />
+    </Tabs.Navigator>
+  );
+};
