ad291edcddc2c6ff7a68cc90e7f12a2bacdf5125
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-07-01 17:57:24 +0900

    movie api

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { movieApi } from "../api";
 
-export default ({ navigation }) => {
+export default () => {
+  const [nowPlaying, setNowPlaying] = useState({
+    movies: [],
+    error: null,
+  });
+  const getData = async () => {
+    const { data, ...rest } = await movieApi.nowPlaying();
+    console.log(rest);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <View style={{ backgroundColor: "black", flex: 1 }}>
       <Text>Movies</Text>
-      <Button title="Detail" onPress={() => navigation.navigate("Detail")} />
     </View>
   );
 };
