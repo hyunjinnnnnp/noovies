0bec5e690e675e22c2649b80b5e51588023fa3f0
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-07-06 13:51:22 +0900

    get data from movie api

--- screens/Movies.js
+++ screens/Movies.js
@@ -3,14 +3,28 @@ import { View, Text } from "react-native";
 import { movieApi } from "../api";
 
 export default () => {
-  const [nowPlaying, setNowPlaying] = useState({
-    movies: [],
-    error: null,
-  });
+  // const [nowPlaying, setNowPlaying] = useState({
+  //   movies: [],
+  //   error: null,
+  // });
   const getData = async () => {
-    const { data, ...rest } = await movieApi.nowPlaying();
-    console.log(rest);
+    const [nowPlaying, error] = await movieApi.nowPlaying();
+    console.log(nowPlaying, error);
+    const [popular, popularError] = await movieApi.popular();
   };
+  // getAnything으로 한번에 모든 함수 통과시킨다
+  //  try{
+  //   const { data: {results} } = await movieApi.nowPlaying();
+  //   setNowPlaying({
+  //     movies: results,
+  //     error: null
+  //   })
+  //  }catch(error){
+  //   setNowPlaying({
+  //     error
+  //   })
+  //  }
+  //};
   useEffect(() => {
     getData();
   }, []);
