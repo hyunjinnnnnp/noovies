6c7d064735edc66bf53deab085675ff4864863a5
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-07-07 16:56:16 +0900

    popular movies: vertical slide

--- components/Movies/Slide.js
+++ components/Movies/Slide.js
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 import PropTypes from "prop-types";
 import { apiImage } from "../../api";
 import Poster from "../Poster";
+import Votes from "../Votes";
 
 const Container = styled.View`
   height: 100%;
@@ -36,11 +37,8 @@ const Title = styled.Text`
   margin-bottom: 10px;
 `;
 
-const Votes = styled.Text`
-  color: rgb(220, 220, 220);
+const VotesContainer = styled.View`
   margin-bottom: 7px;
-  font-size: 12px;
-  font-weight: 500;
 `;
 
 const Overview = styled.Text`
@@ -66,7 +64,9 @@ const Slide = ({ id, title, backgroundImage, votes, overview, poster }) => (
       <Poster url={apiImage(poster)} />
       <Data>
         <Title>{title.length > 40 ? ` ${title.slice(0, 40)}...` : title}</Title>
-        <Votes>⭐️ {votes} / 10</Votes>
+        <VotesContainer>
+          <Votes />
+        </VotesContainer>
         <Overview>{overview.slice(0, 110)}...</Overview>
         <TouchableOpacity>
           <Button>
