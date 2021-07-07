216a7f85e06b3e8d2bf84096e36518d3d8baa3d0
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-07-07 18:10:31 +0900

    upcoming movies: release date

--- components/Horizontal.js
+++ components/Horizontal.js
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 import PropTypes from "prop-types";
 import { TouchableOpacity } from "react-native";
 import Poster from "./Poster";
-import { trimText } from "../utils";
+import { trimText, formatDate } from "../utils";
 
 const Container = styled.View`
   padding: 0px 30px;
@@ -40,7 +40,9 @@ const Horizontal = ({ id, title, poster, overview, releaseDate }) => (
       <Poster url={poster} />
       <Data>
         <Title>{trimText(title, 30)}</Title>
-        {releaseDate ? <ReleaseDate>{releaseDate}</ReleaseDate> : null}
+        {releaseDate ? (
+          <ReleaseDate>{formatDate(releaseDate)}</ReleaseDate>
+        ) : null}
         <Overview>{trimText(overview, 130)}</Overview>
       </Data>
     </Container>
